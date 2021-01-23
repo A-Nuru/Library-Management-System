9ab660ae18d5ad64fed8f358749b549d5c728cfc
Nuru <access2abey@gmail.com>
2021-01-23 16:12:40 +0000

    create exit function

--- Library_Management_Application.py
+++ Library_Management_Application.py
@@ -17,8 +17,34 @@ from twilio.rest import Client
 
 
 
-###########################################################################################
 
+
+
+
+##################                          FRAME-1  (HOME PAGE)                       ##############
+
+
+def Frame1():
+    show_frame(frame1)
+    Label(frame1, text = 'Login as Admin : ', font = ('times',20,'bold italic'),bg='#E0FFFF').place(x=530,y=170)
+    Button(frame1, text = 'Sign In', command = Frame2,bg='#E0FFFF',font = ('times',15,'bold italic')).place(x=800,y=170)
+
+    Label(frame1, text = 'Login as Student : ', font = ('times',20,'bold italic'),bg='#E0FFFF').place(x=530,y=270)
+    Button(frame1, text = 'Sign In', command = Frame2a,bg='#E0FFFF',font = ('times',15,'bold italic')).place(x=800,y=270)
+
+    Label(frame1,text = 'Have  to Register?',font = ('times',20,'bold italic'),bg='#E0FFFF').place(x=530,y=370)
+    Button(frame1, text = 'Sign Up',command = Frame5a,font = ('times',15,'bold italic'),bg='#E0FFFF').place(x=800,y=370)
+
+    Button(frame1, text = 'Exit',command = close,bg='#E0FFFF',font = ('times',15,'bold italic')).place(x=700,y=450)
+
+
+
+##################              FUNCTION TO EXIT THE APPLICATION             ##############
+
+
+def close():
+    root.destroy()
+    
 
 
 ##################                       MAIN WINDOW                         ##############
@@ -32,6 +58,16 @@ root.rowconfigure(0,weight=1)
 root.columnconfigure(0,weight=1)
 
 
+
+
+
+##################                         FRAMES                            ##############
+
+
+frame1 = Frame(root)                                  
+
+
+Frame1()                    # Running first frame
 
 
 root.mainloop()
