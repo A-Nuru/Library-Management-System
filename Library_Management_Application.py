f3979220f123464e45f8d621ad98d46a34638b40
Nuru <access2abey@gmail.com>
2021-01-24 22:42:21 +0000

    create admin login function

--- Library_Management_Application.py
+++ Library_Management_Application.py
@@ -21,21 +21,49 @@ from twilio.rest import Client
 
 
 
+
+
+
+
+
+##################                          FRAME-2  (ADMIN LOGIN PAGE)                       ##############
+
+
+def Frame2():
+    show_frame(frame2)
+    global name_entry
+    global password_entry
+
+    Label(frame2, text = 'Username : ',font = ('times',15,'bold italic'),bg='#FFFFFF').place(x=50,y=170)
+    Label(frame2, text = 'Password : ',font = ('times',15,'bold italic'),bg='#FFFFFF').place(x=50,y=240)
+
+    name_entry=StringVar()
+    Entry(frame2,textvariable=name_entry).place(x=170,y=170)
+
+    password_entry=StringVar()
+    Entry(frame2,textvariable=password_entry, show = '*').place(x=170,y=240)
+    
+    Button(frame2, text = 'Sign In',font = ('times',15,'bold italic'),bg='#FFFFFF').place(x=120,y=320)
+    Button(frame2, text = 'Go Back',font = ('times',15,'bold italic'),bg='#FFFFFF').place(x=120,y=380)
+
+
+
+
 ##################                          FRAME-1  (HOME PAGE)                       ##############
 
 
 def Frame1():
     show_frame(frame1)
     Label(frame1, text = 'Login as Admin : ', font = ('times',20,'bold italic'),bg='#E0FFFF').place(x=530,y=170)
-    Button(frame1, text = 'Sign In', command = Frame2,bg='#E0FFFF',font = ('times',15,'bold italic')).place(x=800,y=170)
+    Button(frame1, text = 'Sign In',bg='#E0FFFF',font = ('times',15,'bold italic')).place(x=800,y=170)
 
     Label(frame1, text = 'Login as Student : ', font = ('times',20,'bold italic'),bg='#E0FFFF').place(x=530,y=270)
-    Button(frame1, text = 'Sign In', command = Frame2a,bg='#E0FFFF',font = ('times',15,'bold italic')).place(x=800,y=270)
+    Button(frame1, text = 'Sign In',bg='#E0FFFF',font = ('times',15,'bold italic')).place(x=800,y=270)
 
     Label(frame1,text = 'Have  to Register?',font = ('times',20,'bold italic'),bg='#E0FFFF').place(x=530,y=370)
-    Button(frame1, text = 'Sign Up',command = Frame5a,font = ('times',15,'bold italic'),bg='#E0FFFF').place(x=800,y=370)
+    Button(frame1, text = 'Sign Up',font = ('times',15,'bold italic'),bg='#E0FFFF').place(x=800,y=370)
 
-    Button(frame1, text = 'Exit',command = close,bg='#E0FFFF',font = ('times',15,'bold italic')).place(x=700,y=450)
+    Button(frame1, text = 'Exit',bg='#E0FFFF',font = ('times',15,'bold italic')).place(x=700,y=450)
 
 
 
@@ -70,13 +98,16 @@ root.columnconfigure(0,weight=1)
 
 
 
-##################                         FRAMES                            ##############
+##################          RENDERING     FRAMES  ON MAIN WINDOW         ##############
 
 
-frame1 = Frame(root)                                  
+frame1 = Frame(root)
+
+frame1.place(relheight=1,relwidth=1)
 
 
-Frame1()                    # Running first frame
+Frame1()                   # Running first frame
+ 
 
 
 root.mainloop()
