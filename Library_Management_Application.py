949c64a8bc9c3700d294e04f5aa25a09c622a457
Nuru <access2abey@gmail.com>
2021-05-30 13:30:30 +0100

    change admin login number

--- Library_Management_Application.py
+++ Library_Management_Application.py
@@ -141,7 +141,7 @@ def admin_Database():
     cursor = conn.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS login (username TEXT , password TEXT, mobile_no TEXT)")
     cursor.execute('DELETE FROM login')
-    cursor.execute("INSERT INTO login (username, password,mobile_no) VALUES('uellibrary', 'uel123',+447388397814)")
+    cursor.execute("INSERT INTO login (username, password,mobile_no) VALUES('uellibrary', 'uel123',+447727161208)")
     conn.commit()
     
     login()
